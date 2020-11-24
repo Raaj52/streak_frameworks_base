f54532a30e8d75cdd34eaec598804db15107870c
Raaj52 <mail2raaj52@gmail.com>
2020-11-24 14:57:56 +0530

    Revert "Disable fingerprint vibration if no haptics"
    This reverts commit 856230e0b79810fe34988c46d0cdb3f1a7617aa7.

--- services/core/java/com/android/server/biometrics/ClientMonitor.java
+++ services/core/java/com/android/server/biometrics/ClientMonitor.java
@@ -298,14 +298,9 @@ public abstract class ClientMonitor extends LoggableMonitor implements IBinder.D
     }
 
     public final void vibrateSuccess() {
-        final boolean isHapticEnabled = Settings.System.getIntForUser(
-                mContext.getContentResolver(), Settings.System.HAPTIC_FEEDBACK_ENABLED, 1,
-                UserHandle.USER_CURRENT) != 0;
-        if (isHapticEnabled) {
-            Vibrator vibrator = mContext.getSystemService(Vibrator.class);
-            if (vibrator != null) {
-                vibrator.vibrate(mSuccessVibrationEffect, FINGERPRINT_SONFICATION_ATTRIBUTES);
-            }
+        Vibrator vibrator = mContext.getSystemService(Vibrator.class);
+        if (vibrator != null) {
+            vibrator.vibrate(mSuccessVibrationEffect, FINGERPRINT_SONFICATION_ATTRIBUTES);
         }
     }
 
